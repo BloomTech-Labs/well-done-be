01b0d3ea9eada82c3cd5c4c39e10ac81c87d64d3
ren.meta@gmail.com <ren.meta@gmail.com>
2019-10-17 14:45:21 -0400

    database is connected to postgres vis sequelize

--- api/server.js
+++ api/server.js
@@ -4,14 +4,80 @@ const cors = require('cors')
 
 const server = express();
 
-const { Client } = require('pg')
-const client = new Client({
-  host: "aa10su4jt2enzmn.cudv4hjvenyx.us-east-2.rds.amazonaws.com",
-  user: "lambda",
-  password: "password",
-  database: "welldone"
-})
-client.connect()
+// const { Client } = require('pg')
+// const client = new Client({
+//   host: "aa10su4jt2enzmn.cudv4hjvenyx.us-east-2.rds.amazonaws.com",
+//   user: "lambda",
+//   password: "password",
+//   database: "welldone"
+// })
+// client.connect()
+
+// const { Pool, Client } = require('pg')
+// const pool = new Pool({
+//   host: "aa10su4jt2enzmn.cudv4hjvenyx.us-east-2.rds.amazonaws.com",
+//   user: "lambda",
+//   password: "password",
+//   database: "welldone",
+//   port: 5432,
+// })
+// pool.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   pool.end()
+// })
+// const client = new Client({
+//   host: "aa10su4jt2enzmn.cudv4hjvenyx.us-east-2.rds.amazonaws.com",
+//   user: "lambda",
+//   password: "password",
+//   database: "welldone",
+//   port: 5432,
+// })
+// client.connect()
+
+// client.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   client.end()
+// })
+
+//---__Works! ----*
+
+// const { Pool, Client } = require('pg')
+// const connectionString = 'postgresql://lambda:password@aa10su4jt2enzmn.cudv4hjvenyx.us-east-2.rds.amazonaws.com:5432/welldone'
+// const pool = new Pool({
+//   connectionString: connectionString,
+// })
+// pool.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   pool.end()
+// })
+// const client = new Client({
+//   connectionString: connectionString,
+// })
+// client.connect()
+// client.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   client.end()
+// })
+
+//----------*
+
+const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('welldone', 'postgres', 'shahid', {
+//   host: 'aa10su4jt2enzmn.cudv4hjvenyx.us-east-2.rds.amazonaws.com',
+//   dialect: 'postgres',
+//   pool: {
+//     max: 9,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
+var sequelize = new Sequelize('postgresql://lambda:password@aa10su4jt2enzmn.cudv4hjvenyx.us-east-2.rds.amazonaws.com:5432/welldone');
+
+sequelize.authenticate().then(() => {
+  console.log("Success!");
+}).catch((err) => {
+  console.log(err);
+});
 
 // middleware
 server.use(express.json());
--- index.js
+++ index.js
@@ -2,6 +2,25 @@ require('dotenv').config();
 const server = require('./api/server.js');
 const defaults = require('./config/defaults')
 
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
 server.listen(defaults.port, () => {
   console.log(`\n*** Server Running on http://localhost:${defaults.port} ***\n`);
 })
