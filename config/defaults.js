a0dfe97e4ffe83618fa3b27ad8eafe920794a814
ren.meta@gmail.com <ren.meta@gmail.com>
2019-10-22 14:16:36 -0400

    cleaned up code from sequelize

--- config/defaults.js
+++ config/defaults.js
@@ -1,10 +1,3 @@
 module.exports = {
-    port: process.env.PORT || 4000,
-    ssl: true
+    port: process.env.PORT || 4000
   };
-
-// //`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
-
-
-// module.exports = { pool }
-// module.exports = { client }
--- index.js
+++ index.js
@@ -2,39 +2,6 @@ require('dotenv').config();
 const server = require('./api/server.js');
 const defaults = require('./config/defaults')
 
-const express = require('express')
-
-// const { Pool, Client } = require('pg')
-// const connectionString = process.env.DATABASE_URL
-// const pool = new Pool({
-//   connectionString: connectionString,
-// })
-// pool.query('SELECT NOW()', (err, res) => {
-//   console.log(err, res)
-//   pool.end()
-// })
-// const client = new Client({
-//   connectionString: connectionString,
-// })
-// client.connect()
-// client.query('SELECT NOW()', (err, res) => {
-//   console.log(err, res)
-//   client.end()
-// })
-
-// const { Client } = require('pg');
-
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true,
-// });
-
-// client.connect();
-// app.listen(port, () => {
-//   console.log(`App running on port ${port}.`)
-// })
-
-
-server.listen(process.env.PORT || defaults.port, () => {
+server.listen(defaults.port, () => {
   console.log(`\n*** Server Running on http://localhost:${defaults.port} ***\n`);
 })
