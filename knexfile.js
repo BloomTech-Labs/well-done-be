28002d222ac743200e69475cfa9e651ecf01e7b7
ren.meta@gmail.com <ren.meta@gmail.com>
2019-10-22 14:32:10 -0400

    changed development to sqlite3 in knexfile for local testing

--- knexfile.js
+++ knexfile.js
@@ -4,43 +4,33 @@
 
 module.exports = {
 
-  development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: __dirname + '/data/migrations',
-    },
-    seeds: { directory: __dirname + '/data/seeds' },
-  },
-  ssl: true,
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './data/auth.sqlite3'
-  //   },
-  //   useNullAsDefault: true,
+  // development: {
+  //   client: 'pg',
+  //   connection: process.env.DATABASE_URL,
   //   migrations: {
-  //     directory: './data/migrations',
+  //     directory: __dirname + '/data/migrations',
   //   },
-  //   seeds: {
-  //     directory: './data/seeds'
-  //   },
-  //   pool: {
-  //     afterCreate: (conn, done) => {
-  //       conn.run('PRAGMA foreign_keys = ON', done);
-  //     }
-  // }
-// },
-
-    // testing: {
-    //   client: 'pg',
-    //   // version: '7.2',
-    //   connection: {
-    //     host : 'aa10su4jt2enzmn.cudv4hjvenyx.us-east-2.rds.amazonaws.com',
-    //     user : 'lambda',
-    //     password : 'password',
-    //     database : 'welldone'
-    //   }
-    // },
+  //   seeds: { directory: __dirname + '/data/seeds' },
+  // },
+  // ssl: true,
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/auth.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+  }
+},
     production: {
       client: 'pg',
       connection: process.env.DATABASE_URL,
@@ -50,13 +40,4 @@ module.exports = {
       seeds: { directory: __dirname + '/data/seeds' },
     },
     ssl: true
-    // production: {
-    //   client: 'pg',
-    //   connection: process.env.DATABASE_URL,
-    //   migrations: {
-    //     directory: __dirname + '/data/migrations',
-    //   },
-    //   seeds: { directory: __dirname + '/data/seeds' },
-    // },
-    // ssl: true
 };
