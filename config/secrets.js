ad7cb52341260a37d2fa9b722ef5fc762b3f3dc4
Julie Thrall <juliethrallstewart@gmail.com>
2019-10-23 08:58:52 -0700

    fixed test config

--- config/secrets.js
+++ config/secrets.js
@@ -1,5 +1,5 @@
 module.exports = {
   jwtSecret: process.env.JWT_SECRET || "keep it secret keep it safe",
   googleApiKey: process.env.GOOGLE_KEY || "fake google api key",
-  environment: process.env.NODE_ENV || "development"
+  environment: process.env.DB_ENV || process.env.NODE_ENV || "development"
 };
