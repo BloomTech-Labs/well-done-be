17595d081a8da9b1ad38a1382f9db9006cca3fb4
LaVon Mejia <lavonmejia@gmail.com>
2019-10-23 14:16:52 -0400

    "consoidated test suites, errors still"
    Co-authored-by: reesekunz <reesekunz21@gmail.com>
    Co-authored-by: Jason Prince <jasoncprince@gmail.com>
    Co-authored-by: Julie Thrall Stewart <juliethrallstewart@gmail.com>
    Co-authored-by: Hong Tran <htran2@babson.edu>

--- api/history/history.router.spec.js
+++ api/history/history.router.spec.js
@@ -1,12 +1,13 @@
 const request = require("supertest");
 const db = require("../../data/dbConfig");
+const History = require("./history.model")
 
 describe("history router", () => {
   beforeEach(async () => {
     await db("history").truncate();
   });
   it("should set environment to testing", () => {
-    expect(process.env.DB_ENV).toBe("testing");
+    expect(process.env.DB_ENV).toBe("test");
   });
 
   it("gets the history endpoint", async done => {
