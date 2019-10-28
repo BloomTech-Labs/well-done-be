9a814bf0b928e4b806d55ce8f4719099f14297ea
ren.meta@gmail.com <ren.meta@gmail.com>
2019-10-28 12:28:43 -0400

    working fetch with padseconds etc

--- services/jsonCache.js
+++ services/jsonCache.js
@@ -33,20 +33,20 @@ async function main() {
 const url =
   "https://dashboard.welldone.org/.netlify/functions/get_momo_status?id="
 async function getPumps() {
-  const { data: config } = await prismic.getDoc("config")
-  const oldData = require("../assets/cache/pumps.json")
-  if (
-    !oldData.lastFetch ||
-    oldData.lastFetch <
-      moment()
-        .subtract(config.update_data, "hours")
-        .unix()
-  ) {
-    console.log("Fetching Pumps Init")
+  // const { data: config } = await prismic.getDoc("config")
+  // we don't want to subtract this data anymore...
+  // const oldData = require("../assets/cache/pumps.json")
+  // if (
+    // // ! oldData.lastFetch ||
+  //   oldData.lastFetch <
+  //     moment()
+  //       .subtract(config.update_data, "hours")
+  //       .unix()
+  // ) {
+  //   console.log("Fetching Pumps Init")
     let pumps = {}
     const prismicPumps = await prismic.getDocs("pump")
     await asyncForEach(prismicPumps.results, async pump => {
-      // console.log(pump.data)
       let village = null
       if (pump.data && pump.data.village.id && !pump.data.village.isBroken) {
         village = await prismic.getVillage(pump.data.village.id)
@@ -70,28 +70,26 @@ async function getPumps() {
         console.log(`${index + 1}/${Object.keys(pumps).length}`)
         const res = await axios.get(`${url}${pump}`)
         let newData = {}
-        // console.log("*****res.data*****", res.data)
         res.data
-        ? res.data.dates.forEach((date, index) => {
-          newData = {
-            ...newData,
-            statuses: {
-              date: date,
-              count: res.data.statuses[index].count,
-              total: res.data.statuses[index].total,
-              status: res.data.statuses[index].status,
-              reportedPercent: res.statuses[index].reportedPercent,
-              pad_counts: res.data.statuses[index].padSeconds.map((item) => item),
-              pad_seconds: res.data.statuses[index].padCounts.map((item) => item),
-            },
-          }
-        })
+          ? res.data.dates.forEach((date, index) => {
+              newData = {
+                ...newData,
+                [date]: {
+                  count: res.data.statuses[index].count,
+                  total: res.data.statuses[index].total,
+                  status: res.data.statuses[index].status,
+                  padCounts: res.data.statuses[index].padCounts,
+                  padSeconds:res.data.statuses[index].padSeconds,
+                  reportedPercent:res.data.statuses[index].reportedPercent
+                },
+              }
+            })
           : {}
         results.push({
           id: pump,
           ...pumps[pump],
           status: res.data.status,
-          date: newData
+          statuses: newData,
         })
       } catch (err) {
         console.error(`Error on pump #${pump}`)
@@ -99,25 +97,26 @@ async function getPumps() {
       }
     })
     console.log("Fetching Pumps Success")
-    // console.log("pumps results*********",{ lastFetch: moment().unix(), pumps: results })
     return { lastFetch: moment().unix(), pumps: results }
-  } else {
-    console.log("Data Up To Date")
-    return oldData
-  }
-}
+  } 
+  // else { 
+  //   console.log("Data Up To Date")
+  //   return oldData
+  // }
+
 
 async function createStore() {
   const oldData = require("../assets/cache/longStore.json")
   const data = require("../assets/cache/pumps.json")
   let pumps = {}
   data.pumps.forEach(({ id, dates, statuses }, index) => {
-    let pumpOldData = oldData.pumps ? oldData.pumps[id] : {}
+
+    // let pumpOldData = oldData.pumps ? oldData.pumps[id] : {}
 
     pumps = {
       ...pumps,
       [id]: {
-        ...pumpOldData,
+        // ...pumpOldData,
         ...data.pumps.find(pump => pump.id === id).statuses,
       },
     }
@@ -131,5 +130,5 @@ async function asyncForEach(array, callback) {
     await callback(array[index], index, array)
   }
 }
-
-main()
+     
+main();
