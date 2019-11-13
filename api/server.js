e056af4dd04599525cabcb76871cabbda65aa6b2
LaVon Mejia <lavonmejia@gmail.com>
2019-11-13 13:00:37 -0500

    commented line 27 back in, added "*"

--- api/server.js
+++ api/server.js
@@ -24,7 +24,7 @@ const padCountsRouter = require("./pad_counts/pad_counts.router")
 
 server.use(express.json());
 server.use(helmet());
-// server.use(cors());
+server.use("*",cors());
 server.use(cors({ credentials: true, origin: `http://localhost:3000/`})); 
 server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
