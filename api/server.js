82ec16c0db5b657fdb70efa09e1cc974a720e8ef
Julie Thrall <juliethrallstewart@gmail.com>
2019-11-15 18:06:36 -0800

    still fixing merge conflicts

--- api/server.js
+++ api/server.js
@@ -3,10 +3,6 @@ const helmet = require("helmet");
 const cors = require("cors");
 const secrets = require("../config/secrets.js");
 const request = require('request')
-<<<<<<< HEAD
-
-=======
->>>>>>> 8321b925d1ade9237b13d4d5da7a6c84a2af52c8
 
 console.log("environment:", secrets.environment);
 const server = express();
@@ -24,7 +20,6 @@ const smsNotificationsRouter = require("./sms_notifications/sms_notifications.ro
 const sensorsRouter = require("./sensors/sensors.router");
 const historyRouter = require("./history/history.router");
 const padSecondsRouter = require("./pad_seconds/pad_seconds.router")     
-<<<<<<< HEAD
 const padCountsRouter = require("./pad_counts/pad_counts.router") 
 const lastFetchRouter = require("./lastFetch/lastFetch.router")  
 
@@ -36,41 +31,12 @@ var reqTimer = setTimeout(function wakeUp() {
 }, 1200000);
 
 reqTimer
-=======
-const padCountsRouter = require("./pad_counts/pad_counts.router")   
-const lastFetchRouter = require("./lastFetch/lastFetch.router")
-   
->>>>>>> 8321b925d1ade9237b13d4d5da7a6c84a2af52c8
+
 
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
-<<<<<<< HEAD
-// server.use(cors({ credentials: true, origin: `http://localhost:3000/`})); 
-// server.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   next();
-// });
-=======
-// server.use(cors({ credentials: true, origin: `http://localhost:3000`})); 
-// server.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-//https://welldone-db.herokuapp.com/
-var reqTimer = setTimeout(function wakeUp() {
-  request("https://welldone-db.herokuapp.com/api/auth/login", function() {
-     console.log("WAKE UP DYNO");
-  });
-  return reqTimer = setTimeout(wakeUp, 1200000);
-}, 1200000);
->>>>>>> 8321b925d1ade9237b13d4d5da7a6c84a2af52c8
-
-reqTimer
 
 server.use("/api/auth", authRouter);
 server.use("/api/orgs", orgRouter);
