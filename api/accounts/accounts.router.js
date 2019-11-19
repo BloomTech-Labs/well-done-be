02dde8ef34c3ee208729e0ee55fb831560b98da0
Julie Thrall <juliethrallstewart@gmail.com>
2019-11-19 11:11:27 -0800

    fixed routes in accounts to not allow a user to enter a duplicate email_address when creating an account, fixed false positives when updating a password

--- api/accounts/accounts.router.js
+++ api/accounts/accounts.router.js
@@ -39,15 +39,20 @@ router.get("/:account_id", authenticate, (req, res) => {
 router.post("/", validateAccount, async (req, res) => {
   try {
     const account = req.body;
-    console.log("account", account);
+    const { email_address } = req.body
     const hash = bcrypt.hashSync(account.password, 10); // 2 ^ n
     account.password = hash;
-    await Accounts.insert(account);
-    const token = generateToken(account);
-    res.status(200).json(token);
+    const isUniqueEmail = await Accounts.findBy({email_address})
+    if (isUniqueEmail === 0) {
+      await Accounts.insert(account);
+      const token = generateToken(account);
+      res.status(200).json(token);
+    } else {
+      res.status(404).json({message: "Email address already taken, please enter a unique email"})
+    }
   } catch (err) {
     console.log(err.message);
-    res.status(400).json(err.message);
+    res.status(500).json(err.message, "Error creating account");
   }
 });
 
@@ -63,18 +68,23 @@ router.put("/:account_id", authenticate, validateUpdate, async (req, res) => {
     res.status(400).json(err.message);
   }
 });
-// TEST SECTION
-router.put("/email/:account_id", authenticate, validateUpdate, async (req, res) => {
+
+// update password only
+router.put("/password/:account_id", authenticate, validateUpdate, async (req, res) => {
   try {
     const { account_id } = req.params;
     const changes = req.body;
     const hash = bcrypt.hashSync(changes.password, 10); // 2 ^ n
     changes.password = hash;
-    await Accounts.update(account_id, changes);
-    res.status(200).json({ message: "Account edited successfully." });
+    const updated = await Accounts.update(account_id, changes);
+    if (updated === 1) {
+    res.status(200).json({ message: "Account edited successfully." })
+    } else {
+      res.status(400).json({ message: "Account not found." })
+    }
   } catch (err) {
     console.log(err.message);
-    res.status(400).json(err.message, "account not found");
+    res.status(500).json(err.message, "Error updating password");
   }
 });
 
