8ecdf4db1a038783b3958461bc94d7f4c8cea069
Gavin Dreyer <gavindreyer1@gmail.com>
2019-12-19 11:21:59 -0800

    no unique constraint on physical_id so had to switch back to id

--- data/migrations/10_tickets.js
+++ data/migrations/10_tickets.js
@@ -9,7 +9,7 @@ exports.up = function(knex) {
 		column
 			.integer('sensor_id')
 			.unsigned()
-			.references('physical_id')
+			.references('id')
 			.inTable('sensors')
 			.onDelete('RESTRICT')
 			.onUpdate('RESTRICT');
