200dd81bc9316a083db8a9246acb43ebd7cb7b00
Gavin Dreyer <gavindreyer1@gmail.com>
2019-12-19 12:05:05 -0800

    removed async

--- api/tickets/tickets.router.js
+++ api/tickets/tickets.router.js
@@ -4,7 +4,7 @@ const Tickets = require('./tickets.model');
 const { validateSensor } = require('../middleware/middleware');
 const { authenticate } = require('../middleware/middleware');
 
-router.get('/', authenticate, async (req, res) => {
+router.get('/', authenticate, (req, res) => {
 	Tickets.find()
 		.then(response => res.status(200).json(response))
 		.catch(err => res.status(500).json({ message: 'Failed to get tickets' }));
