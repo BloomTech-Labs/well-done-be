2e120b10b0d6db222cb2d6dc8116f2bb56571ae0
Luis Martinez <mrluismartinezzz@gmail.com>
2020-01-09 14:17:03 -0800

    modified knexfile && removie sqlite files

--- knexfile.js
+++ knexfile.js
@@ -1,43 +1,46 @@
 // Update with your config settings.
-require('dotenv').config();
+require("dotenv").config();
 // dotenv.config({ path: "./env" });
 
 module.exports = {
-	development: {
-		client: 'pg',
-		useNullAsDefault: true,
-		connection: {
-			database: process.env.DB_DEV_DATABASE,
-			user: process.env.DB_DEV_USER
-		},
-		migrations: {
-			directory: './data/migrations'
-		},
-		seeds: {
-			directory: './data/seeds'
-		}
-	},
+  development: {
+    client: "pg",
+    useNullAsDefault: true,
+    connection: process.env.DB_LOCAL_URL || {
+      database: process.env.DB_LOCAL,
 
-	test: {
-		client: 'sqlite3',
-		connection: {
-			filename: './data/route.sqlite3'
-		},
-		useNullAsDefault: true,
-		migrations: {
-			directory: './data/migrations'
-		},
-		seeds: {
-			directory: './data/seeds'
-		}
-	},
-	production: {
-		client: 'pg',
-		connection: process.env.DATABASE_URL,
-		migrations: {
-			directory: './data/migrations'
-		},
-		seeds: { directory: './data/seeds' }
-	},
-	ssl: true
+      user: process.env.DB_LOCAL_USER,
+
+      password: process.env.DB_LOCAL_PASSWORD
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/route.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: { directory: "./data/seeds" }
+  },
+  ssl: true
 };
