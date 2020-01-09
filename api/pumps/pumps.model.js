31dfb3f84450e936baee05bce7498a6a3aa67187
Gavin Dreyer <gavindreyer1@gmail.com>
2020-01-09 10:38:44 -0800

    returns the object that is posted to the pumps table

--- api/pumps/pumps.model.js
+++ api/pumps/pumps.model.js
@@ -1,38 +1,35 @@
-const knex = require("knex");
-const config = require("../../knexfile");
-const db = require("../../data/dbConfig.js");
+const knex = require('knex');
+const config = require('../../knexfile');
+const db = require('../../data/dbConfig.js');
 
-
-function addPump(pump){
-    return db('pumps')
-            .insert(pump)
-            .returning("id")
+function addPump(pump) {
+	return db('pumps')
+		.insert(pump)
+		.returning('id');
 }
 
-
 // addds multiple pumps to the database
-function addPumps(pumpsArr){
-  pumpsArr.forEach(pump => {
-    return db('pumps')
-    .insert({pump})
-  });
+function addPumps(pumpsArr) {
+	pumpsArr.forEach(pump => {
+		return db('pumps').insert({ pump });
+	});
 }
 
 function findPumps() {
-  return db("pumps as p")
-  .join("organizations as o", "o.id", "p.org_id")
-  .select(
-    "p.id",
-    "p.org_id",
-    "p.country_name as village_name",
-    "p.province_name",
-    "p.commune_name",
-    "p.district_name",
-    "p.latitude",
-    "p.longitude",
-    "o.org_name",
-    "o.headquarter_city"
-  )
+	return db('pumps as p')
+		.join('organizations as o', 'o.id', 'p.org_id')
+		.select(
+			'p.id',
+			'p.org_id',
+			'p.country_name as village_name',
+			'p.province_name',
+			'p.commune_name',
+			'p.district_name',
+			'p.latitude',
+			'p.longitude',
+			'o.org_name',
+			'o.headquarter_city'
+		);
 }
 
 // function getPumps() {
@@ -61,110 +58,108 @@ function findPumps() {
 // }
 
 function getPumpsByOrgId(id) {
-  return db("pumps as p")
-    .join("organizations as o", "o.id", "p.org_id")
-    .join("sensors as s", "s.physical_id", "p.sensor_pid")
-    .where({ org_id:id })
-    .select([
-      "s.id as sensor_index",
-      "s.physical_id",
-      "s.kind",
-      "s.type",
-      "s.cellular",
-      "s.bluetooth",
-      "s.training",
-      "s.remark",
-      "s.data_finished",
-      "s.depth",
-      "s.yield",
-      "s.static",
-      "s.quality",
-      "p.id as pump_index",
-      "p.sensor_pid",
-      "p.org_id",
-      "o.org_name",
-      "o.headquarter_city",
-      "p.country_name as village_name",
-      "p.district_name",
-      "p.province_name",
-      "p.commune_name",
-      "p.latitude",
-      "p.longitude",
-    ])
-    
+	return db('pumps as p')
+		.join('organizations as o', 'o.id', 'p.org_id')
+		.join('sensors as s', 's.physical_id', 'p.sensor_pid')
+		.where({ org_id: id })
+		.select([
+			's.id as sensor_index',
+			's.physical_id',
+			's.kind',
+			's.type',
+			's.cellular',
+			's.bluetooth',
+			's.training',
+			's.remark',
+			's.data_finished',
+			's.depth',
+			's.yield',
+			's.static',
+			's.quality',
+			'p.id as pump_index',
+			'p.sensor_pid',
+			'p.org_id',
+			'o.org_name',
+			'o.headquarter_city',
+			'p.country_name as village_name',
+			'p.district_name',
+			'p.province_name',
+			'p.commune_name',
+			'p.latitude',
+			'p.longitude'
+		]);
 }
 
 function findById(id) {
-  return db("organizations")
-    .where({ id })
-    .first();
+	return db('organizations')
+		.where({ id })
+		.first();
 }
 
 function getPumpById(id) {
-  return db("pumps")
-    .where({ id })
-    .then(pumps => pumps[0]);
-  // .first
+	return db('pumps')
+		.where({ id })
+		.then(pumps => pumps[0]);
+	// .first
 }
 
 function deletePump(id) {
-  return db("pumps")
-    .where({ id })
-    .del();
+	return db('pumps')
+		.where({ id })
+		.del();
 }
 
 const updatePump = (changes, id) => {
-  try {
-    return db("pumps")
-      .where({ id })
-      .update(changes);
-  } catch (err) {
-    console.log(err.message);
-  }
+	try {
+		return db('pumps')
+			.where({ id })
+			.update(changes);
+	} catch (err) {
+		console.log(err.message);
+	}
 };
 
-const getPumpsByVillageName = (filter) => {
-  return db("pumps as p")
-  .join("organizations as o", "o.id", "p.org_id")
-  .join("sensors as s", "s.physical_id", "p.sensor_pid")
-  .where(filter)
-  .select([
-    "p.id as pump_index",
-    "p.org_id",
-    "p.sensor_pid",
-    "p.country_name as village_name",
-    "p.province_name",
-    "p.district_name",
-    "p.commune_name",
-    "p.latitude",
-    "p.longitude",
-    "o.org_name",
-    "o.headquarter_city",
-    "s.id as sensor_index",
-    "s.physical_id",
-    "s.kind",
-    "s.type",
-    "s.cellular",
-    "s.bluetooth",
-    "s.training",
-    "s.remark",
-    "s.data_finished",
-    "s.depth",
-    "s.yield",
-    "s.static",
-    "s.quality",
-  ])
-}
+const getPumpsByVillageName = filter => {
+	return db('pumps as p')
+		.join('organizations as o', 'o.id', 'p.org_id')
+		.join('sensors as s', 's.physical_id', 'p.sensor_pid')
+		.where(filter)
+		.select([
+			'p.id as pump_index',
+			'p.org_id',
+			'p.sensor_pid',
+			'p.country_name as village_name',
+			'p.province_name',
+			'p.district_name',
+			'p.commune_name',
+			'p.latitude',
+			'p.longitude',
+			'o.org_name',
+			'o.headquarter_city',
+			's.id as sensor_index',
+			's.physical_id',
+			's.kind',
+			's.type',
+			's.cellular',
+			's.bluetooth',
+			's.training',
+			's.remark',
+			's.data_finished',
+			's.depth',
+			's.yield',
+			's.static',
+			's.quality'
+		]);
+};
 
 module.exports = {
-    addPumps,
-    addPump,
-    // getPumps,
-    findPumps,
-    getPumpsByOrgId,
-    getPumpById,
-    deletePump,
-    updatePump,
-    getPumpsByVillageName
-}
-
+	addPumps,
+	addPump,
+	// getPumps,
+	findPumps,
+	getPumpsByOrgId,
+	getPumpById,
+	deletePump,
+	updatePump,
+	getPumpsByVillageName
+};
--- api/pumps/pumps.router.js
+++ api/pumps/pumps.router.js
@@ -1,101 +1,107 @@
-const router = require("express").Router();
-const Pumps = require("./pumps.model");
-const { validatePump, validatePumpUpdate } = require("../middleware/middleware");
-const { authenticate } = require("../middleware/middleware");
+const router = require('express').Router();
+const Pumps = require('./pumps.model');
+const {
+	validatePump,
+	validatePumpUpdate
+} = require('../middleware/middleware');
+const { authenticate } = require('../middleware/middleware');
 
 //POST to /api/pumps
-router.post("/", authenticate, validatePump, (req, res) => {
-  const pumpData = req.body;
-  console.log("pumpData", pumpData);
-  Pumps.addPump(pumpData)
-    .then(pump => {
-      res.status(201).json(pump);
-    })
-    .catch(err => {
-      res.status(500).json(err.message);
-    });
+router.post('/', authenticate, validatePump, (req, res) => {
+	const pumpData = req.body;
+	console.log('pumpData', pumpData);
+	Pumps.addPump(pumpData)
+		.then(pump => {
+			Pumps.getPumpById(Number(pump)).then(found => {
+				res.status(201).json(found);
+			});
+		})
+		.catch(err => {
+			res.status(500).json(err.message);
+		});
 });
 
 // GET to /api/pumps
-router.get("/", authenticate, async (req, res) => {
-  try {
-    const pumps = await Pumps.findPumps();
-    res.status(200).json(pumps);
-  } catch (err) {
-    res.status(400).json(err.message);
-  }
+router.get('/', authenticate, async (req, res) => {
+	try {
+		const pumps = await Pumps.findPumps();
+		res.status(200).json(pumps);
+	} catch (err) {
+		res.status(400).json(err.message);
+	}
 });
 
 //GET to /api/pumps/2
-router.get("/:id", authenticate, (req, res) => {
-  const { id } = req.params;
-  Pumps.getPumpById(id)
-    .then(pump => {
-      if (pump) {
-        res.status(200).json(pump);
-      } else res.status(404).json({ message: "pump does not exist" });
-    })
-    .catch(err => res.status(500).json(err.message));
+router.get('/:id', authenticate, (req, res) => {
+	const { id } = req.params;
+	Pumps.getPumpById(id)
+		.then(pump => {
+			if (pump) {
+				res.status(200).json(pump);
+			} else res.status(404).json({ message: 'pump does not exist' });
+		})
+		.catch(err => res.status(500).json(err.message));
 });
 
 // PUT to /api/pumps/1
-router.put("/:id", validatePumpUpdate, authenticate, async (req, res) => {
-  try {
-    const { id } = req.params;
-    const changes = req.body;
-    await Pumps.updatePump(changes, id);
-    res.status(200).json({ message: "Pump edited successfully." });
-  } catch (err) {
-    console.log(err.message);
-    res.status(400).json(err.message);
-  }
+router.put('/:id', validatePumpUpdate, authenticate, async (req, res) => {
+	try {
+		const { id } = req.params;
+		const changes = req.body;
+		await Pumps.updatePump(changes, id);
+		const updatedPump = await Pumps.getPumpById(id);
+		res.status(200).json(updatedPump);
+	} catch (err) {
+		console.log(err.message);
+		res.status(400).json(err.message);
+	}
 });
 
 //DELETE to /api/pumps/6
-router.delete("/:id", authenticate, (req, res) => {
-  const { id } = req.params;
-  console.log("id", id);
-  Pumps.getPumpById(id)
-    .then(pump => {
-      console.log(pump);
-      if (pump) {
-        Pumps.deletePump(id)
-          .then(count => {
-            console.log("fired");
-            res.status(200).json({ message: `deleted ${count} pump` });
-          })
-          .catch(err => res.status(500).json(err));
-      } else {
-        res.status(404).json({ message: "pump does not exist" });
-      }
-    })
-    .catch(err => res.status(500).json(err.message));
+router.delete('/:id', authenticate, (req, res) => {
+	const { id } = req.params;
+	console.log('id', id);
+	Pumps.getPumpById(id)
+		.then(pump => {
+			console.log(pump);
+			if (pump) {
+				Pumps.deletePump(id)
+					.then(count => {
+						console.log('fired');
+						res.status(200).json({ message: `deleted ${count} pump` });
+					})
+					.catch(err => res.status(500).json(err));
+			} else {
+				res.status(404).json({ message: 'pump does not exist' });
+			}
+		})
+		.catch(err => res.status(500).json(err.message));
 });
 
 //GET to /api/pumps/org/2
-router.get("/org/:id", authenticate, (req, res) => {
-  const { id } = req.params;
-  Pumps.getPumpsByOrgId(id)
-    .then(pumps => {
-      console.log("pumps", pumps);
-      res.status(200).json(pumps);
-    })
-    .catch(err => {
-      res.status(500).json({ message: "Fail to retrieve orgs" });
-    });
+router.get('/org/:id', authenticate, (req, res) => {
+	const { id } = req.params;
+	Pumps.getPumpsByOrgId(id)
+		.then(pumps => {
+			console.log('pumps', pumps);
+			res.status(200).json(pumps);
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Fail to retrieve orgs' });
+		});
 });
 
-router.post("/village_name", authenticate, (req, res) => {
-  let { village_name } = req.body;
-  console.log(village_name, "this is the village name")
-  Pumps.getPumpsByVillageName({country_name: village_name})
-    .then(pumps => {
-      console.log("pumps", pumps);
-      res.status(200).json(pumps);
-    })
-    .catch(err => {
-      res.status(500).json({ message: "Fail to retrieve orgs" });
-    });
+router.post('/village_name', authenticate, (req, res) => {
+	let { village_name } = req.body;
+	console.log(village_name, 'this is the village name');
+	Pumps.getPumpsByVillageName({ country_name: village_name })
+		.then(pumps => {
+			console.log('pumps', pumps);
+			res.status(200).json(pumps);
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Fail to retrieve orgs' });
+		});
 });
 
 module.exports = router;
