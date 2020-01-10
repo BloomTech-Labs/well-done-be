0f77c7ac4784b23918c9c311e3db1ed7417f7a73
Luis Martinez <mrluismartinezzz@gmail.com>
2020-01-10 10:27:31 -0800

    deleted extra file & merged migration file into one

--- data/migrations/20200109093717_010_accounts_on_cascade.js
+++ data/migrations/20200109093717_010_accounts_on_cascade.js
@@ -1,12 +1,35 @@
-
 exports.up = function(knex) {
-    return knex.schema.table("accounts", async (tbl) => {
-        await tbl.dropColumn("org_id")
-});
-}
-
-exports.down = function(knex) {
-    return knex.schema.table("accounts", async (tbl) => {
-        await tbl.integer("org_id")
+  return Promise.all([
+    knex.schema.table("accounts", tbl => tbl.dropColumn("org_id")),
+    knex.schema.table("accounts", tbl => {
+      tbl
+        .integer("org_id")
+        .unsigned()
+        .references("id")
+        .inTable("organizations")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
     })
+  ]);
+};
+
+exports.down = async function(knex) {
+  const tableExists = await knex.schema.hasTable("accounts");
+  if (tableExists) {
+    return Promise.all([
+      knex.schema.table(
+        "accounts",
+        async tbl => await tbl.dropColumn("org_id")
+      ),
+      knex.schema.table("accounts", async tbl => {
+        await tbl
+          .integer("org_id")
+          .unsigned()
+          .references("id")
+          .inTable("organizations")
+          .onDelete("RESTRICT")
+          .onUpdate("RESTRICT");
+      })
+    ]);
+  }
 };
