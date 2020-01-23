a3df1e95e0923b5884747193bba76d8383a339f7
Gavin Dreyer <gavindreyer1@gmail.com>
2020-01-22 18:02:51 -0800

    returning most recent dates sensor and its relative information associated with the operators id

--- api/operators/operators.model.js
+++ api/operators/operators.model.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
 const config = require('../../knexfile');
 const db = require('../../data/dbConfig.js');
+const moment = require('moment');
 
 function getOperators() {
 	return db('operators');
@@ -32,8 +33,14 @@ function getAssignedSensors() {
 		]);
 }
 
-function getAssignedSensorsByOperatorId(id) {
-	return db('sensors_and_operators as so')
+async function getAssignedSensorsByOperatorId(id) {
+	let operator_id = id;
+
+	let today = new Date();
+	today = moment(today).format('ddd MMM D YYYY');
+
+	//getting all sensors associated with the id of the operator and their info
+	let getSensorsInfo = await db('sensors_and_operators as so')
 		.join('sensors as s', 's.physical_id', 'so.sensor_id')
 		.join('operators as o', 'o.id', 'so.operator_id')
 		.where('o.id', id)
@@ -51,6 +58,42 @@ function getAssignedSensorsByOperatorId(id) {
 			'p.latitude',
 			'p.longitude'
 		]);
+
+	//getting just the sensor id
+	let getSensors = await db('sensors_and_operators as so')
+		.where({ operator_id })
+		.select(['so.sensor_id']);
+
+	//getting history
+	let recentHistory = await db('history');
+
+	//filtering out all entries not associated with the sensors assigned to the operator or the current date
+	recentHistory = recentHistory.filter(history => {
+		return getSensors.find(sensor => {
+			if (
+				history.sensor_id === sensor.sensor_id &&
+				(history.date === today || history.date === null)
+			) {
+				return history;
+			}
+		});
+	});
+
+	let sensorHistoryAndInfo = [];
+
+	//creating a new object with both relevant information and recent information
+	getSensorsInfo.forEach(sensorInfo => {
+		recentHistory.forEach(history => {
+			if (history.sensor_id === sensorInfo.sensor_id) {
+				sensorHistoryAndInfo = [
+					...sensorHistoryAndInfo,
+					{ ...history, ...sensorInfo }
+				];
+			}
+		});
+	});
+
+	return sensorHistoryAndInfo;
 }
 
 function findBy(filter) {
