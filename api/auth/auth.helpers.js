4c96600f781ed8b5bdb13c754df4c3655aac1939
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-02-03 17:34:16 -0500

    adding user email in token

--- api/auth/auth.helpers.js
+++ api/auth/auth.helpers.js
@@ -1,17 +1,31 @@
-const jwt = require('jsonwebtoken');
-const secrets = require('../../config/secrets');
+const jwt = require("jsonwebtoken");
+const secrets = require("../../config/secrets");
+const Operators = require("../operators/operators.model");
 
-const generateToken = user => {
-	const payload = {
-		id: user.id,
-		email: user.email_address
-	};
-	const options = {
-		expiresIn: '1d'
-	};
-	return jwt.sign(payload, secrets.jwtSecret, options);
+const generateToken = async user => {
+  if (user["role"] === "operator") {
+    const operator = await Operators.getOperatorByEmail(user.email_address);
+    const payload = {
+      id: user.id,
+      email: user.email_address,
+      operator_id: operator.id
+    };
+    const options = {
+      expiresIn: "1d"
+    };
+
+    return jwt.sign(payload, secrets.jwtSecret, options);
+  } else {
+    const payload = {
+      id: user.id,
+      email: user.email_address
+    };
+    const options = {
+      expiresIn: "1d"
+    };
+    return jwt.sign(payload, secrets.jwtSecret, options);
+  }
 };
-
 module.exports = {
-	generateToken
+  generateToken
 };
