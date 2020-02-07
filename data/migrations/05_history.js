c51f8bba1b49eba814eb23b76945c4441fda7812
Gavin Dreyer <gavindreyer1@gmail.com>
2020-02-07 12:49:14 -0800

    new table migration

--- data/migrations/05_history.js
+++ data/migrations/05_history.js
@@ -1,17 +1,24 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("history", column => {
-    column.increments();
-    column.timestamp("created_at").defaultTo(knex.fn.now())
-    column.string("date").nullable();
-    column.integer("count").nullable();
-    column.bigInteger("total").nullable();
-    column.integer("status").nullable();
-    column.integer("sensor_id");
-    column.float("reported_percent").nullable();
-
-  });
+	return knex.schema.createTable('history', column => {
+		column.increments();
+		column.timestamp('created_at').defaultTo(knex.fn.now());
+		column.string('date').nullable();
+		column.integer('count').nullable();
+		column.bigInteger('total').nullable();
+		column.integer('status').nullable();
+		column.integer('sensor_id');
+		column.float('reported_percent').nullable();
+		column.integer('pad_count_0');
+		column.integer('pad_count_1');
+		column.integer('pad_count_2');
+		column.integer('pad_count_3');
+		column.integer('pad_seconds_0');
+		column.integer('pad_seconds_1');
+		column.integer('pad_seconds_2');
+		column.integer('pad_seconds_3');
+	});
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("history");
+	return knex.schema.dropTableIfExists('history');
 };
