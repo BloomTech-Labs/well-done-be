64c6d4ff848f3038800ab49d1a6c4c54bff04bf7
Gavin Dreyer <gavindreyer1@gmail.com>
2020-06-03 12:24:19 -0700

    added new router to get recent history

--- api/history/history.model.js
+++ api/history/history.model.js
@@ -3,102 +3,128 @@ const config = require("../../knexfile");
 const db = require("../../data/dbConfig.js");
 
 const find = () => {
-  try {
-    return db("history as h")
-    .join("pad_seconds as s", "s.history_id", "h.id")
-    .join("pad_counts as c", "c.history_id", "h.id")
-  } catch (err) {
-    console.log(err.message);
-  }
+	try {
+		return db("history as h")
+			.join("pad_seconds as s", "s.history_id", "h.id")
+			.join("pad_counts as c", "c.history_id", "h.id");
+	} catch (err) {
+		console.log(err.message);
+	}
+};
+
+const findRecent = async () => {
+	try {
+		let history = await db("history");
+
+		let historyObj = {};
+
+		history.forEach((item, index) => {
+			if (!historyObj[item.sensor_id]) {
+				historyObj[item.sensor_id] = [item.status];
+
+				for (let i = 1; i < 13; i++) {
+					historyObj[item.sensor_id].push(history[index + i].status);
+				}
+
+				if (historyObj[item.sensor_id].includes(2)) {
+					historyObj[item.sensor_id] = "yes";
+				} else {
+					historyObj[item.sensor_id] = "no";
+				}
+			}
+		});
+
+		return historyObj;
+	} catch (err) {
+		console.log(err.message);
+	}
 };
 
 const findById = id => {
-  try {
-    return db("history as h")
-      .where({ id })
-      .first();
-  } catch (err) {
-    console.log(err.message);
-  }
+	try {
+		return db("history as h")
+			.where({ id })
+			.first();
+	} catch (err) {
+		console.log(err.message);
+	}
 };
 
-const padCounts = (id) => {
-  return db("pad_counts")
-  .where({history_id: id})
-}
+const padCounts = id => {
+	return db("pad_counts").where({ history_id: id });
+};
 
-const padSeconds = (id) => {
-  return db("pad_seconds")
-  .where({history_id: id})
-}
-
+const padSeconds = id => {
+	return db("pad_seconds").where({ history_id: id });
+};
 
 function getHistoryById(id) {
-  const historyQuery = findById(id);
-  const getPadCounts = padCounts(id);
-  const getPadSeconds = padSeconds(id);
-  return Promise.all([historyQuery, getPadCounts, getPadSeconds]).then(
-    ([history, counts, seconds]) => {
-      history.pad_counts = counts;
-      history.pad_seconds = seconds;
-      return history;
-    }
-  );
+	const historyQuery = findById(id);
+	const getPadCounts = padCounts(id);
+	const getPadSeconds = padSeconds(id);
+	return Promise.all([historyQuery, getPadCounts, getPadSeconds]).then(
+		([history, counts, seconds]) => {
+			history.pad_counts = counts;
+			history.pad_seconds = seconds;
+			return history;
+		}
+	);
 }
 
 function getHistoryBySensorId(id) {
-  return db("history as h")
-  .join("pad_seconds as s", "s.history_id", "h.id")
-  .join("pad_counts as c", "c.history_id", "h.id")
-  .where({sensor_id: id})
+	return db("history as h")
+		.join("pad_seconds as s", "s.history_id", "h.id")
+		.join("pad_counts as c", "c.history_id", "h.id")
+		.where({ sensor_id: id });
 }
 
 const insert = async historical => {
-  try {
-    await db("history").insert(historical);
-    console.log("History successfully created!");
-  } catch (err) {
-    console.log(err);
-  }
+	try {
+		await db("history").insert(historical);
+		console.log("History successfully created!");
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 const update = async (changes, id) => {
-  try {
-    changes
-      ? await db("history")
-          .where({ id })
-          .first()
-          .update(changes)
-      : null;
-  } catch (err) {
-    console.log(err);
-  }
+	try {
+		changes
+			? await db("history")
+					.where({ id })
+					.first()
+					.update(changes)
+			: null;
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 const remove = async id => {
-  try {
-    const historical = findById(id);
-    if (historical) {
-      const deleted = await db("history")
-        .where({ id })
-        .del();
-      return deleted;
-    } else {
-      console.log(
-        "There was an error finding an history with the provided id."
-      );
-    }
-  } catch (err) {
-    console.log(err.message);
-  }
+	try {
+		const historical = findById(id);
+		if (historical) {
+			const deleted = await db("history")
+				.where({ id })
+				.del();
+			return deleted;
+		} else {
+			console.log(
+				"There was an error finding an history with the provided id."
+			);
+		}
+	} catch (err) {
+		console.log(err.message);
+	}
 };
 
 module.exports = {
-  find,
-  findById,
-  insert,
-  update,
-  remove,
-  getHistoryById,
-  getHistoryBySensorId
+	find,
+	findRecent,
+	findById,
+	insert,
+	update,
+	remove,
+	getHistoryById,
+	getHistoryBySensorId
 };
--- api/history/history.router.js
+++ api/history/history.router.js
@@ -5,94 +5,104 @@ const { validateHistory } = require("../middleware/middleware");
 
 // GET to /api/history
 router.get("/", authenticate, (req, res) => {
-  History.find()
-    .then(history => {
-      res.status(200).json(history);
-    })
-    .catch(err => {
-      res.status(500).json({ message: err.message });
-    });
+	History.find()
+		.then(history => {
+			res.status(200).json(history);
+		})
+		.catch(err => {
+			res.status(500).json({ message: err.message });
+		});
+});
+
+// GET recent history /api/history/recent
+router.get("/recent", async (req, res) => {
+	try {
+		let recentHistory = await History.findRecent();
+		res.status(200).json(recentHistory);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 });
 
 //GET to /api/history/1
 router.get("/:id", authenticate, (req, res) => {
-  const { id } = req.params;
-  History.getHistoryById(id)
-    .then(history => {
-      if (history) {
-        res.json(history);
-      } else {
-        res
-          .status(404)
-          .json({ message: "Could not find history with given id." });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: "Failed to get history" });
-    });
+	const { id } = req.params;
+	History.getHistoryById(id)
+		.then(history => {
+			if (history) {
+				res.json(history);
+			} else {
+				res
+					.status(404)
+					.json({ message: "Could not find history with given id." });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ message: "Failed to get history" });
+		});
 });
 
 router.get("/sensor_id/:id", authenticate, (req, res) => {
-  const { id } = req.params;
-  History.getHistoryBySensorId(id)
-    .then(history => {
-      if (history) {
-        res.json(history);
-      } else {
-        res
-          .status(404)
-          .json({ message: "Could not find history with given id." });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: "Failed to get history" });
-    });
+	const { id } = req.params;
+	History.getHistoryBySensorId(id)
+		.then(history => {
+			if (history) {
+				res.json(history);
+			} else {
+				res
+					.status(404)
+					.json({ message: "Could not find history with given id." });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ message: "Failed to get history" });
+		});
 });
 
 // POST to /api/history
 router.post("/", authenticate, validateHistory, (req, res) => {
-  const historyData = req.body;
+	const historyData = req.body;
 
-  History.insert(historyData)
-    .then(history => {
-      res.status(201).json(history);
-    })
-    .catch(err => {
-      res.status(500).json({ message: err.message });
-    });
+	History.insert(historyData)
+		.then(history => {
+			res.status(201).json(history);
+		})
+		.catch(err => {
+			res.status(500).json({ message: err.message });
+		});
 });
 
 // PUT to /api/history 2
 router.put("/:id", authenticate, validateHistory, (req, res) => {
-  const { id } = req.params;
-  const changes = req.body;
-  History.findById(id)
-    .then(history => {
-      if (history) {
-        History.update(changes, id).then(changes => {
-          res.json(changes);
-        });
-      } else {
-        res
-          .status(404)
-          .json({ message: "Could not find history with given id" });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: "Failed to update history" });
-    });
+	const { id } = req.params;
+	const changes = req.body;
+	History.findById(id)
+		.then(history => {
+			if (history) {
+				History.update(changes, id).then(changes => {
+					res.json(changes);
+				});
+			} else {
+				res
+					.status(404)
+					.json({ message: "Could not find history with given id" });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ message: "Failed to update history" });
+		});
 });
 
 // DELETE to /api/history/3
 router.delete("/:id", authenticate, async (req, res) => {
-  try {
-    const { id } = req.params;
-    await History.remove(id);
-    res.status(200).json(id);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).json(err.message);
-  }
+	try {
+		const { id } = req.params;
+		await History.remove(id);
+		res.status(200).json(id);
+	} catch (err) {
+		console.log(err.message);
+		res.status(500).json(err.message);
+	}
 });
 
 module.exports = router;
