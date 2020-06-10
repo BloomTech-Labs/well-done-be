2e4a32367119ebda2915fd5fd99dd3c417b7d34f
Gavin Dreyer <gavindreyer1@gmail.com>
2020-06-10 09:19:47 -0700

    returning cord_email in api/sensors/ route

--- api/sensors/sensors.model.js
+++ api/sensors/sensors.model.js
@@ -1,165 +1,166 @@
-const knex = require('knex');
-const config = require('../../knexfile');
-const db = require('../../data/dbConfig.js');
+const knex = require("knex");
+const config = require("../../knexfile");
+const db = require("../../data/dbConfig.js");
 
 function addSensor(sensor) {
-	return db('sensors')
+	return db("sensors")
 		.insert(sensor)
-		.returning('id');
+		.returning("id");
 }
 
 function findSensors() {
-	return db('sensors');
+	return db("sensors");
 }
 
 function getSensorById(id) {
-	return db('sensors')
+	return db("sensors")
 		.where({ id })
 		.then(sensors => sensors[0]);
 }
 
 function getSensorBySensorId(id) {
 	return (
-		db('sensors as s')
+		db("sensors as s")
 			// .join("history as h", "s.physical_id", "h.sensor_id")
 			.where({ physical_id: id })
 	);
 }
 
 function getSensorByOrgId(org_id) {
-	return db('sensors')
-		.join('pumps', 'sensors.pump_id', 'pumps.id')
-		.where('pumps.org_id', org_id)
-		.join('organizations as o', 'o.id', 'p.org_id')
+	return db("sensors")
+		.join("pumps", "sensors.pump_id", "pumps.id")
+		.where("pumps.org_id", org_id)
+		.join("organizations as o", "o.id", "p.org_id")
 		.select([
-			's.id as sensor_index',
-			's.physical_id',
-			's.kind',
-			's.type',
-			's.cellular',
-			's.bluetooth',
-			's.training',
-			's.remark',
-			's.data_finished',
-			's.depth',
-			's.yield',
-			's.static',
-			's.quality',
-			'p.id as pump_index',
-			'p.sensor_pid',
-			'p.org_id',
-			'o.org_name',
-			'o.headquarter_city',
-			'p.country_name as village_name',
-			'p.district_name',
-			'p.province_name',
-			'p.commune_name',
-			'p.latitude',
-			'p.longitude'
+			"s.id as sensor_index",
+			"s.physical_id",
+			"s.kind",
+			"s.type",
+			"s.cellular",
+			"s.bluetooth",
+			"s.training",
+			"s.remark",
+			"s.data_finished",
+			"s.depth",
+			"s.yield",
+			"s.static",
+			"s.quality",
+			"p.id as pump_index",
+			"p.sensor_pid",
+			"p.org_id",
+			"o.org_name",
+			"o.headquarter_city",
+			"p.country_name as village_name",
+			"p.district_name",
+			"p.province_name",
+			"p.commune_name",
+			"p.latitude",
+			"p.longitude"
 		])
 		.then(sensors => sensors[0]);
 }
 
 function updateSensor(id, change) {
-	return db('sensors')
+	return db("sensors")
 		.where({ id })
 		.update(change);
 }
 
 function deleteSensor(id) {
-	return db('sensors')
+	return db("sensors")
 		.where({ id })
 		.del();
 }
 
 function getSensorNPump() {
-	return db('sensors as s')
-		.join('pumps as p', 'p.sensor_pid', 's.physical_id')
-		.join('organizations as o', 'o.id', 'p.org_id')
+	return db("sensors as s")
+		.join("pumps as p", "p.sensor_pid", "s.physical_id")
+		.join("organizations as o", "o.id", "p.org_id")
 		.select([
-			's.id as sensor_index',
-			's.physical_id',
-			's.kind',
-			's.type',
-			's.cellular',
-			's.bluetooth',
-			's.training',
-			's.remark',
-			's.data_finished',
-			's.depth',
-			's.yield',
-			's.static',
-			's.quality',
-			'p.id as pump_index',
-			'p.sensor_pid',
-			'p.org_id',
-			'o.org_name',
-			'o.headquarter_city',
-			'p.country_name as village_name',
-			'p.district_name',
-			'p.province_name',
-			'p.commune_name',
-			'p.latitude',
-			'p.longitude'
+			"s.id as sensor_index",
+			"s.physical_id",
+			"s.kind",
+			"s.type",
+			"s.cellular",
+			"s.bluetooth",
+			"s.training",
+			"s.remark",
+			"s.data_finished",
+			"s.depth",
+			"s.yield",
+			"s.static",
+			"s.quality",
+			"s.cord_email",
+			"p.id as pump_index",
+			"p.sensor_pid",
+			"p.org_id",
+			"o.org_name",
+			"o.headquarter_city",
+			"p.country_name as village_name",
+			"p.district_name",
+			"p.province_name",
+			"p.commune_name",
+			"p.latitude",
+			"p.longitude"
 		]);
 }
 
 //convert this to promises to return entire object
 function getSensorNPumpNHistory() {
-	return db('sensors as s')
-		.join('pumps as p', 'p.sensor_pid', 's.physical_id')
-		.join('history as h', 'h.sensor_id', 's.physical_id');
+	return db("sensors as s")
+		.join("pumps as p", "p.sensor_pid", "s.physical_id")
+		.join("history as h", "h.sensor_id", "s.physical_id");
 }
 
 function findSensorsAndHistories() {
-	return db('sensors as s')
-		.join('history as h', 's.physical_id', 'h.sensor_id')
-		.join('pad_counts as pc', 'pc.history_id', 'h.id')
-		.join('pad_seconds as ps', 'ps.history_id', 'h.id')
-		.join('pumps as p', 's.physical_id', 'p.sensor_pid')
-		.join('organizations as o', 'o.id', 'p.org_id')
-		.orderBy('h.created_at', 'asc')
+	return db("sensors as s")
+		.join("history as h", "s.physical_id", "h.sensor_id")
+		.join("pad_counts as pc", "pc.history_id", "h.id")
+		.join("pad_seconds as ps", "ps.history_id", "h.id")
+		.join("pumps as p", "s.physical_id", "p.sensor_pid")
+		.join("organizations as o", "o.id", "p.org_id")
+		.orderBy("h.created_at", "asc")
 		.select([
-			's.id as sensor_index',
-			's.physical_id',
-			's.kind',
-			's.type',
-			's.cellular',
-			's.bluetooth',
-			's.training',
-			's.remark',
-			's.data_finished',
-			's.depth',
-			's.yield',
-			's.static',
-			's.quality',
-			'p.id as pump_index',
-			'p.sensor_pid',
-			'p.org_id',
-			'o.org_name',
-			'o.headquarter_city',
-			'p.country_name as village_name',
-			'p.district_name',
-			'p.province_name',
-			'p.commune_name',
-			'p.latitude',
-			'p.longitude',
-			'h.id as history_index',
-			'h.date',
-			'h.created_at',
-			'h.count',
-			'h.total',
-			'h.status',
-			'h.sensor_id',
-			'h.reported_percent',
-			'pc.pad_count_0',
-			'pc.pad_count_1',
-			'pc.pad_count_2',
-			'pc.pad_count_3',
-			'ps.pad_seconds_0',
-			'ps.pad_seconds_1',
-			'ps.pad_seconds_2',
-			'ps.pad_seconds_3'
+			"s.id as sensor_index",
+			"s.physical_id",
+			"s.kind",
+			"s.type",
+			"s.cellular",
+			"s.bluetooth",
+			"s.training",
+			"s.remark",
+			"s.data_finished",
+			"s.depth",
+			"s.yield",
+			"s.static",
+			"s.quality",
+			"p.id as pump_index",
+			"p.sensor_pid",
+			"p.org_id",
+			"o.org_name",
+			"o.headquarter_city",
+			"p.country_name as village_name",
+			"p.district_name",
+			"p.province_name",
+			"p.commune_name",
+			"p.latitude",
+			"p.longitude",
+			"h.id as history_index",
+			"h.date",
+			"h.created_at",
+			"h.count",
+			"h.total",
+			"h.status",
+			"h.sensor_id",
+			"h.reported_percent",
+			"pc.pad_count_0",
+			"pc.pad_count_1",
+			"pc.pad_count_2",
+			"pc.pad_count_3",
+			"ps.pad_seconds_0",
+			"ps.pad_seconds_1",
+			"ps.pad_seconds_2",
+			"ps.pad_seconds_3"
 		])
 		.then(res => {
 			let compare = [];
@@ -177,54 +178,54 @@ function findSensorsAndHistories() {
 }
 
 function findSensorsAndHistoriesByOrgId(org_id) {
-	return db('sensors as s')
-		.join('history as h', 's.physical_id', 'h.sensor_id')
-		.join('pad_counts as pc', 'pc.history_id', 'h.id')
-		.join('pad_seconds as ps', 'ps.history_id', 'h.id')
-		.join('pumps as p', 's.physical_id', 'p.sensor_pid')
-		.join('organizations as o', 'o.id', 'p.org_id')
-		.orderBy('h.created_at', 'asc')
+	return db("sensors as s")
+		.join("history as h", "s.physical_id", "h.sensor_id")
+		.join("pad_counts as pc", "pc.history_id", "h.id")
+		.join("pad_seconds as ps", "ps.history_id", "h.id")
+		.join("pumps as p", "s.physical_id", "p.sensor_pid")
+		.join("organizations as o", "o.id", "p.org_id")
+		.orderBy("h.created_at", "asc")
 		.select([
-			's.id as sensor_index',
-			's.physical_id',
-			's.kind',
-			's.type',
-			's.cellular',
-			's.bluetooth',
-			's.training',
-			's.remark',
-			's.data_finished',
-			's.depth',
-			's.yield',
-			's.static',
-			's.quality',
-			'p.id as pump_index',
-			'p.sensor_pid',
-			'p.org_id',
-			'o.org_name',
-			'o.headquarter_city',
-			'p.country_name as village_name',
-			'p.district_name',
-			'p.province_name',
-			'p.commune_name',
-			'p.latitude',
-			'p.longitude',
-			'h.id as history_index',
-			'h.date',
-			'h.created_at',
-			'h.count',
-			'h.total',
-			'h.status',
-			'h.sensor_id',
-			'h.reported_percent',
-			'pc.pad_count_0',
-			'pc.pad_count_1',
-			'pc.pad_count_2',
-			'pc.pad_count_3',
-			'ps.pad_seconds_0',
-			'ps.pad_seconds_1',
-			'ps.pad_seconds_2',
-			'ps.pad_seconds_3'
+			"s.id as sensor_index",
+			"s.physical_id",
+			"s.kind",
+			"s.type",
+			"s.cellular",
+			"s.bluetooth",
+			"s.training",
+			"s.remark",
+			"s.data_finished",
+			"s.depth",
+			"s.yield",
+			"s.static",
+			"s.quality",
+			"p.id as pump_index",
+			"p.sensor_pid",
+			"p.org_id",
+			"o.org_name",
+			"o.headquarter_city",
+			"p.country_name as village_name",
+			"p.district_name",
+			"p.province_name",
+			"p.commune_name",
+			"p.latitude",
+			"p.longitude",
+			"h.id as history_index",
+			"h.date",
+			"h.created_at",
+			"h.count",
+			"h.total",
+			"h.status",
+			"h.sensor_id",
+			"h.reported_percent",
+			"pc.pad_count_0",
+			"pc.pad_count_1",
+			"pc.pad_count_2",
+			"pc.pad_count_3",
+			"ps.pad_seconds_0",
+			"ps.pad_seconds_1",
+			"ps.pad_seconds_2",
+			"ps.pad_seconds_3"
 		])
 		.where({ org_id })
 		.then(res => {
@@ -243,61 +244,61 @@ function findSensorsAndHistoriesByOrgId(org_id) {
 }
 
 function getSensorNHistoryByPhysicalId(id) {
-	return db('sensors as s')
-		.join('pumps as p', 's.physical_id', 'p.sensor_pid')
+	return db("sensors as s")
+		.join("pumps as p", "s.physical_id", "p.sensor_pid")
 		.where({ physical_id: id });
 }
 
 function findSensorsAndHistoriesBySensorsPhysicalId(id) {
-	return db('sensors as s')
-		.join('history as h', 's.physical_id', 'h.sensor_id')
-		.join('pad_counts as pc', 'pc.history_id', 'h.id')
-		.join('pad_seconds as ps', 'ps.history_id', 'h.id')
-		.join('pumps as p', 's.physical_id', 'p.sensor_pid')
-		.join('organizations as o', 'o.id', 'p.org_id')
+	return db("sensors as s")
+		.join("history as h", "s.physical_id", "h.sensor_id")
+		.join("pad_counts as pc", "pc.history_id", "h.id")
+		.join("pad_seconds as ps", "ps.history_id", "h.id")
+		.join("pumps as p", "s.physical_id", "p.sensor_pid")
+		.join("organizations as o", "o.id", "p.org_id")
 		.where({ physical_id: id })
-		.orderBy('h.created_at', 'asc')
+		.orderBy("h.created_at", "asc")
 		.select([
-			's.id as sensor_index',
-			's.physical_id',
-			's.kind',
-			's.type',
-			's.cellular',
-			's.bluetooth',
-			's.training',
-			's.remark',
-			's.data_finished',
-			's.depth',
-			's.yield',
-			's.static',
-			's.quality',
-			'p.id as pump_index',
-			'p.sensor_pid',
-			'p.org_id',
-			'o.org_name',
-			'o.headquarter_city',
-			'p.country_name as village_name',
-			'p.district_name',
-			'p.province_name',
-			'p.commune_name',
-			'p.latitude',
-			'p.longitude',
-			'h.id as history_index',
-			'h.date',
-			'h.created_at',
-			'h.count',
-			'h.total',
-			'h.status',
-			'h.sensor_id',
-			'h.reported_percent',
-			'pc.pad_count_0',
-			'pc.pad_count_1',
-			'pc.pad_count_2',
-			'pc.pad_count_3',
-			'ps.pad_seconds_0',
-			'ps.pad_seconds_1',
-			'ps.pad_seconds_2',
-			'ps.pad_seconds_3'
+			"s.id as sensor_index",
+			"s.physical_id",
+			"s.kind",
+			"s.type",
+			"s.cellular",
+			"s.bluetooth",
+			"s.training",
+			"s.remark",
+			"s.data_finished",
+			"s.depth",
+			"s.yield",
+			"s.static",
+			"s.quality",
+			"p.id as pump_index",
+			"p.sensor_pid",
+			"p.org_id",
+			"o.org_name",
+			"o.headquarter_city",
+			"p.country_name as village_name",
+			"p.district_name",
+			"p.province_name",
+			"p.commune_name",
+			"p.latitude",
+			"p.longitude",
+			"h.id as history_index",
+			"h.date",
+			"h.created_at",
+			"h.count",
+			"h.total",
+			"h.status",
+			"h.sensor_id",
+			"h.reported_percent",
+			"pc.pad_count_0",
+			"pc.pad_count_1",
+			"pc.pad_count_2",
+			"pc.pad_count_3",
+			"ps.pad_seconds_0",
+			"ps.pad_seconds_1",
+			"ps.pad_seconds_2",
+			"ps.pad_seconds_3"
 		])
 		.then(res => {
 			let compare = [];
